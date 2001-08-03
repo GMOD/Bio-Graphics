7bbf690ed5abd7b485892be3a12c47ff66eaec74
Allen Day <allenday@users.sourceforge.net>
2001-08-03 16:04:25 +0000

    benign change

--- test.pl
+++ test.pl
@@ -2,7 +2,7 @@
 # `make test'. After `make install' it should work as `perl test.pl'
 
 ######################### We start with some black magic to print on failure.
-
+#
 # Change 1..1 below to 1..last_test_to_print .
 # (It may become useful if the test is moved to ./t subdirectory.)
 
