d543bce34e8458375c41d9a34c16218f7dc112eb
Lincoln Stein <lincoln.stein@gmail.com>
2002-04-12 04:28:15 +0000

    * This is the same version that is in BioPerl version 1.01. * Fixes to handle case of a transcript glyph that is zoomed in so far that only an intron shows (no exons).

--- Graphics.pm
+++ Graphics.pm
@@ -4,7 +4,7 @@ use Bio::Graphics::Panel;
 use strict;
 
 use vars '$VERSION';
-$VERSION = '1.02';
+$VERSION = '1.03';
 
 1;
 
--- Graphics/FeatureFile.pm
+++ Graphics/FeatureFile.pm
@@ -1,6 +1,6 @@
 package Bio::Graphics::FeatureFile;
 
-# $Id: FeatureFile.pm,v 1.22 2002-04-01 04:29:57 lstein Exp $
+# $Id: FeatureFile.pm,v 1.23 2002-04-12 04:28:15 lstein Exp $
 
 # This package parses and renders a simple tab-delimited format for features.
 # It is simpler than GFF, but still has a lot of expressive power.
